9b6922ce6555bca1c9f6705222db42f3810bead9
Gabby Zapata <gabby.ecuador@gmail.com>
2023-12-09 17:45:48 -0500

    Comienzo app gifs

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interaces/gifs.interfaces';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  public gifsList: Gif[] = [];
+
+  private _tagsHistory: string[] = [];
+  private apiKey:string = 'LaAthCIwyAqaORilFpFsmG14iV8Q4KsP';
+  private serviceUlr:string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) { }
+
+  get tagsHIstory() {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+  }
+
+  public searchTag(tag:string):void {
+
+    if(tag.length === 0) return;
+
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
+    this.http.get<SearchResponse>(`${this.serviceUlr}/search`, {params})
+      .subscribe( resp => {
+        this.gifsList = resp.data;
+      });
+  }
+
+}
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+
+  constructor(private gifsService: GifsService){}
+
+  get tags(): string[] {
+    return this.gifsService.tagsHIstory;
+  }
+
+  searchTag(tag:string):void {
+    this.gifsService.searchTag(tag);
+  }
+
+}

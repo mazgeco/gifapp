220d01919356e6fcd9019bc67be7e282dd46facb
Gabby Zapata <gabby.ecuador@gmail.com>
2023-12-10 11:51:44 -0500

    Fin de la sección 6

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,9 @@ export class GifsService {
   private apiKey:string = 'LaAthCIwyAqaORilFpFsmG14iV8Q4KsP';
   private serviceUlr:string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+   }
 
   get tagsHIstory() {
     return [...this._tagsHistory];
@@ -24,7 +26,20 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0,10);
+    this.saveLocalStorage();
   }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if(!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
+  }
+
 
   public searchTag(tag:string):void {
 
